5edb89055eca32df5ba7b179bee1cec7453d3ddc
Svyatoslav <blackdbf@gmail.com>
2024-10-21 20:26:12 +0300

    init app

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      appHost: process.env.APP_HOST,
+        }
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
+      link: [],
+      script: []
+    }
+  },
+  components: [
+        {
+            path: '~/components',
+            pathPrefix: false
+        }
+  ],
+  css: [
+    '~/assets/sass/app.scss',
+  ],
+  modules: []
+})
